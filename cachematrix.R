7655da3f5344489b0c815098369b8bd18ca53d22
JustinJoseCab <100875299+JustinJoseCab@users.noreply.github.com>
2022-03-29 09:52:18 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,9 @@
 ## This function behaves as described by the assignement instructions.
 
-## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
 
-#set the value of the matrix
-#get the value of the matrix
+#set the value of the vector
+#get the value of the vector
 #set the value of the inverse
 #get the value of the inverse
 
@@ -24,7 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## The following function calculates the inverse of the special 
-## "matrix" created with the above function. However, it first 
+## "vector" created with the above function. However, it first 
 ## checks to see if the inverse has already been calculated. If so, it gets the
 ## inverse from the cache and skips the computation. Otherwise, it 
 ## calculates the inverse of the data and sets the value of the inverse 
